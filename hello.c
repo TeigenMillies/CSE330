7c9e3e9421e5168d7195cce7d322b8ecd02d6fd9
TeigenMillies <143120897+TeigenMillies@users.noreply.github.com>
2024-02-05 05:37:07 +0000

    hello.c

--- hello.c
+++ hello.c
@@ -1,29 +1,23 @@
 #include <linux/module.h>     /* Needed by all modules */ 
 #include <linux/kernel.h>     /* Needed for KERN_INFO */ 
 #include <linux/init.h>       /* Needed for the macros */ 
+
+//make the variable then pass to hello.c
+static int Intparameter = 0;
+hello(Intparameter, int, 0644);
+
+static char *charparameter = "default";
+hello(charparameter, charp, 0644);
   
-///< The license type -- this affects runtime behavior 
-MODULE_LICENSE("GPL"); 
-  
-///< The author -- visible when you use modinfo 
-MODULE_AUTHOR("Akshat Sinha"); 
-  
-///< The description -- see modinfo 
-MODULE_DESCRIPTION("A simple Hello world LKM!"); 
-  
-///< The version of the module 
-MODULE_VERSION("0.1"); 
-  
-static int __init hello_start(void) 
+static int __init developer_info (void) 
 { 
-    printk(KERN_INFO "Loading hello module...\n"); 
-    printk(KERN_INFO "Hello world\n"); 
+    printk(KERN_INFO "Hello, I am Teigen Millies, a student of CSE330 %s %d\n", charparameter, Intparameter); 
     return 0; 
 } 
   
 static void __exit hello_end(void) 
 { 
-    printk(KERN_INFO "Goodbye Mr.\n"); 
+    printk(KERN_INFO "Goodbye-Teigen Millies\n"); 
 } 
   
 module_init(hello_start); 
